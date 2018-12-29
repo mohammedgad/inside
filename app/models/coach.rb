7f2c64411b628589bbda60ca7b5ccf2b016a51f4
Mohammed Gad <mohammedgad7@gmail.com>
2018-12-29 03:01:06 +0200

    add student before save callback assign to coach

--- app/models/coach.rb
+++ app/models/coach.rb
@@ -3,9 +3,13 @@ class Coach < ApplicationRecord
   has_and_belongs_to_many :students
 
   #Validations
+  validates_uniqueness_of :name, on: [:create, :update], message: "must be unique"
   validates_presence_of :name, on: [:create, :update], message: "can't be blank"
   validates_presence_of :cap, on: [:create, :update], message: "can't be blank"
   validate :check_cap, on: [:create, :update]
+
+  #Scopes
+  scope :high_cap, -> { where(cap: Coach.maximum(:cap)).first }
 
   #Instance Methods
   def check_cap
--- app/models/student.rb
+++ app/models/student.rb
@@ -4,4 +4,28 @@ class Student < ApplicationRecord
 
   #Validations
   validates_presence_of :name, on: [:create, :update], message: "can't be blank"
+  validates_uniqueness_of :name, on: [:create, :update], message: "must be unique"
+
+  #Callbacks
+  before_save :assign_to_coach
+
+  #Instance Methods
+  def assign_to_coach
+    high_cap = Coach.high_cap
+    high_cap_students = high_cap.students.count
+    assigned = false
+    Coach.where.not(id: high_cap.id).order(cap: :desc).each do |c|
+      current_cap = c.cap
+      perfect_num = (high_cap_students * current_cap) / high_cap.cap #Number of students the coach should have
+      if c.students.count < perfect_num
+        self.coaches << c
+        assigned = true
+        break
+      end
+    end
+    if !assigned
+      self.coaches << high_cap
+    end
+  end
+
 end
